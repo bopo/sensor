9bde3cd224e6c8ddb4816a265f60d9cb32291e1d
Travis CI <travis@travis-ci.org>
2018-03-18 14:32:35 +0800

    initail

--- script/install.sh
+++
@@ -1,6 +0,0 @@
-curl -s http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key | sudo apt-key add -
-
-sudo wget http://repo.mosquitto.org/debian/mosquitto-jessie.list -P /etc/apt/sources.list.d/
-
-sudo apt-get update
-sudo apt-get install mosquitto
