7c00b6d32c1f6b0ad76b1b05ea04de994c1488f2
Travis CI <travis@travis-ci.org>
2018-03-24 23:35:39 +0800

    first commit

---
+++ script/clean_mosquitto_auth_plugin.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# cd $DIR/mosquitto-auth-plug
+# mv auth-plug.so /etc/mosquitto/
+
+# cd ..
+rm -fr mosquitto-auth-plug org.eclipse.mosquitto
+
+# cp $DIR/auth_plugin.conf /etc/mosquitto/conf.d/auth_plugin.conf
--- script/configure_mosquitto_auth_plugin.sh
+++ script/configure_mosquitto_auth_plugin.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#!/usr/bin/env sh
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR/mosquitto-auth-plug
-sudo mv auth-plug.so /etc/mosquitto/
-cd ..
-rm -fr mosquitto-auth-plug org.eclipse.mosquitto
+mkdir -p /etc/mosquitto/conf.d/
+# cd $(pwd)/mosquitto-auth-plug
+cp $(pwd)/mosquitto-auth-plug/auth-plug.so /etc/mosquitto/
 
-sudo cp $DIR/auth_plugin.conf /etc/mosquitto/conf.d/auth_plugin.conf
+# cd ..
+# rm -fr mosquitto-auth-plug org.eclipse.mosquitto
+
+cp $(pwd)/auth_plugin.conf /etc/mosquitto/conf.d/auth_plugin.conf
