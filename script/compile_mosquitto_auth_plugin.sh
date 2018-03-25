d649dff5d80e66df36120b01d864a2d0c9cc571c
Travis CI <travis@travis-ci.org>
2018-03-25 10:42:31 +0800

    first commit

--- script/compile_mosquitto_auth_plugin.sh
+++ script/compile_mosquitto_auth_plugin.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# sudo apt-get install mosquitto libssl-dev libmosquitto-dev libcurl4-openssl-dev -y
-apk add --no-cache --virtual .build-deps curl-dev openssl-dev gcc g++ automake autoconf make git
-apk del .fetch-deps
+sudo apt-get install mosquitto libssl-dev libmosquitto-dev libcurl4-openssl-dev -y
+# apk add --no-cache --virtual .build-deps curl-dev openssl-dev gcc g++ automake autoconf make git
+# apk del .fetch-deps
 
 git clone http://git.eclipse.org/gitroot/mosquitto/org.eclipse.mosquitto.git
 git clone https://github.com/jpmens/mosquitto-auth-plug.git
@@ -15,7 +15,7 @@ make
 
 cd -
 
-mkdir -p /etc/mosquitto/conf.d/
+# mkdir -p /etc/mosquitto/conf.d/
 # cd $(pwd)/mosquitto-auth-plug
 cp $(pwd)/mosquitto-auth-plug/auth-plug.so /etc/mosquitto/
 
--- server/sensor/mosquitto/auth_plugin/urls.py
+++ server/sensor/mosquitto/auth_plugin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('acl', views.Acl.as_view(), name='mqtt_acl'),
-    path('auth', views.Auth.as_view(), name='mqtt_auth'),
+    path('getuser', views.Auth.as_view(), name='mqtt_auth'),
+    path('aclcheck', views.Acl.as_view(), name='mqtt_acl'),
     path('superuser', views.Superuser.as_view(), name='mqtt_superuser'),
 ]
